9a8b09cf8abab50ba70e621a10c7b659b69f2299
Sudhansh1234 <100071590+Sudhansh1234@users.noreply.github.com>
2025-09-14 23:53:31 +0530

    Delete index.ts

--- api/index.ts
+++
@@ -1,9 +0,0 @@
-import { createServer } from '../server/index';
-import serverless from 'serverless-http';
-
-// Create the Express app
-const app = createServer();
-
-// Export the serverless handler
-export default serverless(app);
-
